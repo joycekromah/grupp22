f6d505e4d59224114d6bc351166231633ad2a215
isakhakola <isak.hakola98@gmail.com>
2025-01-10 11:23:37 +0100

    Update NewsAPI.py

--- NewsAPI.py
+++ NewsAPI.py
@@ -1,23 +1,35 @@
+
 import requests
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
 
 def main():
     searchWord = input("Ange sökord: ")
     posts = get_articles(searchWord)
-    format_output(posts)
+    if posts:
+        format_output(posts)
 
 
 def get_articles(searchWord):
+    # Retrieve the API key from the environment variables
+    api_key = os.getenv('NEWS_API_KEY')
+    if not api_key:
+        print("Error: API key not found. Make sure it's set in the .env file.")
+        return None
 
-     url = f'https://newsapi.org/v2/everything?q={searchWord}&pageSize=10&apiKey=7113466387ef4dc29321b66a334c094f'
+    url = f'https://newsapi.org/v2/everything?q={searchWord}&pageSize=10&apiKey={api_key}'
 
-     response = requests.get(url)
+    response = requests.get(url)
 
-     if response.status_code == 200:
-         posts = response.json()
-         return posts
-     else:
-         print(f"Error: {response.status_code}")
-         return None
+    if response.status_code == 200:
+        posts = response.json()
+        return posts
+    else:
+        print(f"Error: {response.status_code}")
+        return None
 
 
 def format_output(posts):
